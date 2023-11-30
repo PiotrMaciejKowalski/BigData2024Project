fca429b6e1abea5e7e3154d3fdcc5d82e8dae068
PiotrMaciejKowalski <piotr.maciej.kowalski.developer@gmail.com>
2023-11-30 17:28:42 +0100

    add stripped

--- stripped/colabs/Analiza_szeregow_czasowych_dot_roslinnosci_stripped.py
+++ stripped/colabs/Analiza_szeregow_czasowych_dot_roslinnosci_stripped.py
@@ -1,5 +1,5 @@
 """
-<a href="https://colab.research.google.com/github/PiotrMaciejKowalski/BigData2024Project/blob/Analiza-szeregow-czasowych-dot-roslinnosci/colabs/Analiza_szereg%C3%B3w_czasowych_dot_ro%C5%9Blinno%C5%9Bci.ipynb" target="_parent"><img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab"/></a>
+<a href="https://colab.research.google.com/github/PiotrMaciejKowalski/BigData2024Project/blob/Analiza-szeregow-czasowych-dot-roslinnosci/colabs/Analiza_szeregow_czasowych_dot_roslinnosci.ipynb" target="_parent"><img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab"/></a>
 """
 
 """
@@ -30,6 +30,7 @@ import matplotlib.gridspec as gridspec
 import pandas as pd
 from google.colab import drive
 from pyspark.sql import SparkSession
+from pyspark.sql import DataFrame as SparkDataFrame
 from pyspark.sql.types import IntegerType, FloatType, StringType, StructType
 
 
@@ -55,8 +56,8 @@ drive.mount('/content/drive')
 Wczytanie danych NASA znajdujących się na dysku w sparku:
 """
 
-columns = ['lon', 'lat', 'Date', 'SWdown', 'LWdown', 'SWnet', 'LWnet', 'Qle', 'Qh', 'Qg', 'Qf', 'Snowf', 'Rainf', 'Evap', 'Qs', 'Qsb', 'Qsm', 'AvgSurfT', 'Albedo', 'SWE', 'SnowDepth', 'SnowFrac', 'SoilT_0_10cm', 'SoilT_10_40cm', 
-           'SoilT_40_100cm', 'SoilT_100_200cm', 'SoilM_0_10cm', 'SoilM_10_40cm', 'SoilM_40_100cm', 'SoilM_100_200cm', 'SoilM_0_100cm', 'SoilM_0_200cm', 'RootMoist', 'SMLiq_0_10cm', 'SMLiq_10_40cm', 'SMLiq_40_100cm', 'SMLiq_100_200cm', 
+columns = ['lon', 'lat', 'Date', 'SWdown', 'LWdown', 'SWnet', 'LWnet', 'Qle', 'Qh', 'Qg', 'Qf', 'Snowf', 'Rainf', 'Evap', 'Qs', 'Qsb', 'Qsm', 'AvgSurfT', 'Albedo', 'SWE', 'SnowDepth', 'SnowFrac', 'SoilT_0_10cm', 'SoilT_10_40cm',
+           'SoilT_40_100cm', 'SoilT_100_200cm', 'SoilM_0_10cm', 'SoilM_10_40cm', 'SoilM_40_100cm', 'SoilM_100_200cm', 'SoilM_0_100cm', 'SoilM_0_200cm', 'RootMoist', 'SMLiq_0_10cm', 'SMLiq_10_40cm', 'SMLiq_40_100cm', 'SMLiq_100_200cm',
            'SMAvail_0_100cm', 'SMAvail_0_200cm', 'PotEvap', 'ECanop', 'TVeg', 'ESoil', 'SubSnow', 'CanopInt', 'ACond', 'CCond', 'RCS', 'RCT', 'RCQ', 'RCSOL', 'RSmin','RSMacr', 'LAI', 'GVEG', 'Streamflow']
 
 # Utworzenie schematu określającego typ zmiennych
@@ -101,7 +102,7 @@ nasa_ym.createOrReplaceTempView("nasa_ym")
 """
 
 # Ograniczenie zbioru nasa do wartości, które potrzebujemy, ustawienie pomiaru zawsze na 1 dzień miesiąca
-time_series = spark.sql("""
+GVEG_SparkDataFrame = spark.sql("""
                         SELECT
                         lon, lat,
                         to_date(CONCAT(Year, '-', Month, '-1')) as Date, GVEG
@@ -121,37 +122,53 @@ distinct_wsp.shape
 
 
 # FIXME przenieść do katalogu src / @see (https://app.clickup.com/t/86bwj0nt4)
-def time_series_GVEG(lon: float, lat: float):
+def extract_time_series_from_dataframe(sdf: SparkDataFrame, lon: float, lat: float) -> pd.DataFrame:
   """
-  Funkcja generująca wykres w czasie wartości zmiennej GVEG (green vegetation) dla zadanych współrzednych.
-
+  Funkcja przekształcająca zadany SparkDataFrame w szereg czasowy dla zadanych współrzednych lon, lat.
   Parametry:
+  - df (SparkDataFrame): ramka danych w Sparku zawierająca następujace kolumny: lon, lat, Date (pomiar ustawiony na pierwszy dzien miesiąca), atrybuty
   - lon (float): długość geograficzna
   - lat (float): szerokość geograficzna
   """
   # ograniczenie zbioru do konkretnej pary współrzędnych
-  series = time_series.filter((time_series['lon'] == lon) & (time_series['lat'] == lat)).drop('lon', 'lat')
+  time_series = sdf.filter((sdf['lon'] == lon) & (sdf['lat'] == lat))
   # Przejście na pandas
-  time_series_Pandas = series.toPandas()
+  time_series_Pandas = time_series.toPandas()
   # Ustawienie 'date' jako indeksu
   time_series_Pandas.set_index('Date', inplace=True)
+  return time_series_Pandas
+
+
+# Test
+GVEG_time_series = extract_time_series_from_dataframe(GVEG_SparkDataFrame, -89.4375, 32.1875)
+
+
+# FIXME przenieść do katalogu src / @see (https://app.clickup.com/t/86bwj0nt4)
+def plot_time_series(df: pd.DataFrame, atrybut: str) -> None:
+  """
+  Funkcja generująca wykres w czasie dla zadanego atrybutu.
+  Parametry:
+  - df (DataFrame): szereg czasowy; Pandas DataFrame
+  - atrybut (str): atrybut, dla którego chcemy zrobić wykres w czasie
+  """
   # Obliczanie 12-miesięcznej średniej kroczącej
-  time_series_Pandas['12m_MA'] = time_series_Pandas['GVEG'].rolling(window=12).mean()
+  df['12m_MA'] = df[atrybut].rolling(window=12).mean()
   # Tworzenie wykresu
   plt.figure(figsize=(11,6))
   gs = gridspec.GridSpec(2, 1, height_ratios=[2, 1])
-  xmin, xmax = time_series_Pandas.index.min() - pd.DateOffset(years=1) , time_series_Pandas.index.max() + pd.DateOffset(years=1)
+  xmin, xmax = df.index.min() - pd.DateOffset(years=1) , df.index.max() + pd.DateOffset(years=1)
+
   ax1 = plt.subplot(gs[0])
-  ax1.plot(time_series_Pandas.index, time_series_Pandas['GVEG']) 
-  ax1.plot(time_series_Pandas.index, time_series_Pandas['12m_MA'], label='12-miesięczna średnia krocząca', color='red')
-  ax1.set_title(f'Wykres Szeregu Czasowego (lon {lon}, lat {lat})')
+  ax1.plot(df.index, df[atrybut])
+  ax1.plot(df.index, df['12m_MA'], label='12-miesięczna średnia krocząca', color='red')
+  ax1.set_title(f"Wykres Szeregu Czasowego dla lon = {df['lon'].unique()}, lat = {df['lat'].unique()}")
   ax1.legend(loc='upper left')
-  ax1.set_ylabel('Wartość GVEG')
+  ax1.set_ylabel(f"Wartość {atrybut}")
   ax1.set_xlim(xmin, xmax)
   ax1.grid(True)
 
   ax2 = plt.subplot(gs[1])
-  ax2.plot(time_series_Pandas.index, time_series_Pandas['12m_MA'], label='12-miesięczna średnia krocząca', color='red')
+  ax2.plot(df.index, df['12m_MA'], label='12-miesięczna średnia krocząca', color='red')
   ax2.set_xlabel('Data')
   ax2.grid(True)
   ax2.set_xlim(xmin, xmax)
@@ -160,13 +177,18 @@ def time_series_GVEG(lon: float, lat: float):
   plt.show()
 
 
+# Test
+plot_time_series(GVEG_time_series, 'GVEG')
+
+
 # losujemy 10 par wspołrzednych ze zbioru nasa
-random = distinct_wsp.sample(10)
+random = distinct_wsp.sample(10, random_state=123)
 
 
 # sprawdzenie szeregu czasowego dla wylosowanych współrzędnych
 for index, row in random.iterrows():
-  time_series_GVEG(row['lon'], row['lat'])
+  GVEG_time_series = extract_time_series_from_dataframe(GVEG_SparkDataFrame, row['lon'], row['lat'])
+  plot_time_series(GVEG_time_series, 'GVEG')
 
 
 # pustynia mojave i jej "obrzeża" (ograniczenie terenu do współrzednych wytyczających skrajne krańce pustyni)
@@ -174,12 +196,13 @@ mojave = distinct_wsp[(distinct_wsp['lon'] >= -116.15) &  (distinct_wsp['lon']  
 
 
 # losujemy 10 współrzednych z zakresu pustyni mojave i jej obrzeży
-random_mojave = mojave.sample(10)
+random_mojave = mojave.sample(10, random_state=123)
 
 
 # sprawdzenie szeregu czasowego dla wylosowanych współrzędnych
 for index, row in random_mojave.iterrows():
-  time_series_GVEG(row['lon'], row['lat'])
+  GVEG_time_series = extract_time_series_from_dataframe(GVEG_SparkDataFrame, row['lon'], row['lat'])
+  plot_time_series(GVEG_time_series, 'GVEG')
 
 
 
