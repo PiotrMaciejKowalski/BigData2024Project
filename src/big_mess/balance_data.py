a6c6c54b973d7456a130b92d8dbd6afcf5214543
unknown <rafal.gwizdon98@gmail.com>
2023-12-24 15:53:18 +0100

    Poprawka

--- src/big_mess/balance_data.py
+++ src/big_mess/balance_data.py
@@ -4,26 +4,28 @@ from imblearn.over_sampling import RandomOverSampler, SMOTE
 
 class BalanceDataSet():
   '''
-  Two techniques for handling imbalanced data. 
+  Two techniques for handling imbalanced data.
   '''
   def __init__(
       self,
       X: DataFrame,
       y: DataFrame
       ) -> None:
-    assert len(X)==len(y)
-  
+      self.X = X
+      self.y = y
+      assert len(self.X)==len(self.y)
+
   def useOverSampling(
       self,
       randon_seed: Optional[int] = 2023
       ) -> Tuple[DataFrame, DataFrame]:
     oversample = RandomOverSampler( sampling_strategy='auto',
                   random_state=randon_seed)
-    return oversample.fit_resample(X, y)
-    
+    return oversample.fit_resample(self.X, self.y)
+
   def useSMOTE(
       self,
       randon_seed: Optional[int] = 2023
       ) -> Tuple[DataFrame, DataFrame]:
     smote = SMOTE(random_state=randon_seed)
-    return smote.fit_resample(X, y)
+    return smote.fit_resample(self.X, self.y)
