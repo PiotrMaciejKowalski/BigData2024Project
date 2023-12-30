04075eb87a617242dc676d0e854a57b75f025186
PiotrMaciejKowalski <piotr.maciej.kowalski.developer@gmail.com>
2023-12-30 17:59:40 +0100

    try add stripped

--- stripped/colabs/Wczytywanie_danych_NASA_w_sparku_stripped.py
+++ stripped/colabs/Wczytywanie_danych_NASA_w_sparku_stripped.py
@@ -1,15 +1,3 @@
-"""
-<a href="https://colab.research.google.com/github/PiotrMaciejKowalski/BigData2024Project/blob/Szablon-wczytywania-danych-w-sparku/colabs/Wczytywanie_danych_NASA_w_sparku.ipynb" target="_parent"><img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab"/></a>
-"""
-
-"""
-# Wczytywanie danych w sparku
-"""
-
-"""
-Utworzenie środowiska pyspark do obliczeń:
-"""
-
 !apt-get install openjdk-8-jdk-headless -qq > /dev/null
 !wget -q dlcdn.apache.org/spark/spark-3.5.0/spark-3.5.0-bin-hadoop3.tgz
 !tar xf spark-3.5.0-bin-hadoop3.tgz
@@ -30,28 +18,13 @@ from pyspark.sql.types import IntegerType, FloatType, StringType, StructType
 
 from google.colab import drive
 
-
-"""
-Utowrzenie sesji:
-"""
-
 spark = SparkSession.builder\
         .master("local")\
         .appName("Colab")\
         .config('spark.ui.port', '4050')\
         .getOrCreate()
 
-
-"""
-Połączenie z dyskiem:
-"""
-
 drive.mount('/content/drive')
-
-
-"""
-Wczytanie danych NASA znajdujących się na dysku w sparku:
-"""
 
 # Wczytanie zbioru sampled w celu pobrania nazw kolumn
 sampled = pd.read_csv('/content/drive/MyDrive/BigMess/NASA/sampled_NASA_200k.csv')
@@ -64,22 +37,11 @@ for i in sampled.columns:
   else:
     schemat = schemat.add(i, FloatType(), True)
 
-
 # Wczytanie zbioru Nasa w sparku
 nasa = spark.read.format('csv').option("header", True).schema(schemat).load('/content/drive/MyDrive/BigMess/NASA/NASA.csv')
 nasa.show(5)
 
-
-"""
-Zanim zacznimy pisać kwerendy należy jeszcze dodać nasz DataFrame (df) do "przestrzeni nazw tabel" Sparka:
-"""
-
 nasa.createOrReplaceTempView("nasa")
-
-
-"""
-Rodzielenie kolumny "Date" na kolumny "Year" oraz "Month"
-"""
 
 nasa = spark.sql("""
           SELECT
@@ -89,10 +51,7 @@ nasa = spark.sql("""
           FROM nasa n
           """)
 
-
 nasa = nasa.drop("Date")
 nasa.show(5)
 
-
-
-
+3+3
